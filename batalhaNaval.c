76abf5de4d19a1d411a343a8cad1ba131a06e44a
Jairo Bragança <115939809+JairoBraganca@users.noreply.github.com>
2026-03-10 05:33:38 +0000

    initial commit

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,7 +5,7 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Naviosj
+    // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
